5785103e625347536047a476c0b06c04e917c303
benoit <benoit.fabre@highhive.de>
2017-03-21 19:14:37 +0100

    Add attempt to connect with wrong cert

--- 03-setup-mariadb-server.sh
+++ 03-setup-mariadb-server.sh
@@ -10,5 +10,5 @@ else
 fi
 
 docker run --name sslmaria -v $PWD:/etc/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mariadb \
---ssl-ca=/etc/mysql/ca-cert/ca-cert.pem --ssl-cert=/etc/mysql/server-cert/server-cert.pem \
---ssl-key=/etc/mysql/server-private/server-private-key-rsa.pem  --bind-address=*
+--ssl-ca=/etc/mysql/ca-cert-1/ca-cert-1.pem --ssl-cert=/etc/mysql/server-cert-1/server-cert-1.pem \
+--ssl-key=/etc/mysql/server-private-1/server-private-key-rsa-1.pem  --bind-address=*
